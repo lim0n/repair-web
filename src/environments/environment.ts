7b827b80886e9aa62a711e5115f6008738e4efd3
Anatoly Mishin <lim0ncmon@gmail.com>
2026-06-05 19:47:56 +0300

    docker

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,4 @@
 export const environment = {
-  // apiUrl: 'http://localhost:3000',
-  // apiUrl: 'http://localhost'
-  apiUrl: process.env['API_URL'],
-  apiPathPrefix: process.env['API_PATH_PREFIX'],  
+  apiUrl: process.env['API_URL'] || 'http://localhost:3000',
+  apiPathPrefix: process.env['API_PATH_PREFIX'] || '',  
 };
