2b7a2c4f7ac11e67551fec01253db9e410d8769d
Anatoly Mishin <lim0ncmon@gmail.com>
2026-06-21 14:44:30 +0300

    страница 404

--- src/app/pages/service-pages/not-found-page/not-found-page.ts
+++ src/app/pages/service-pages/not-found-page/not-found-page.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Logo } from '@components/logo/logo';
 
 @Component({
   selector: 'app-not-found-page',
-  imports: [],
+  imports: [
+    RouterLink,
+    Logo
+  ],
   templateUrl: './not-found-page.html',
   styleUrl: './not-found-page.scss',
+  host: { class: 'not-found-page' },
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NotFoundPage {
 
